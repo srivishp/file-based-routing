5bc381798052d9486d0cfb0df768314ab1cc3cf1
Sri Vishnu <srivishp@gmail.com>
2023-11-15 14:26:33 +0530

    Add 404.js page

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+// Next JS will detect any 404 error that occurs and automatically show this page
+//* The file should be named as 404.js
+function PageNotFound() {
+  return <h1>Page Not Found :/</h1>;
+}
+
+export default PageNotFound;
