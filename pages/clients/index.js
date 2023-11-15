eb74d3706f100bec6a6aacab9b3f7f51c39f1eae
Sri Vishnu <srivishp@gmail.com>
2023-11-15 12:30:07 +0530

    Navigate with Link for Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
 function ClientsPage() {
-  return <h1>The Clients Page</h1>;
+  const clients = [
+    { id: "AMZ", name: "Amazon" },
+    { id: "GGL", name: "Google" },
+    { id: "MSFT", name: "Microsoft" },
+  ];
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {/* Here the [clientIdSlug] takes Google, Amazon & Microsoft as concrete values*/}
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ClientsPage;
