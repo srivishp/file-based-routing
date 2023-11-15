671691c822da1f400f90e4cc88b8d127311721bf
Sri Vishnu <srivishp@gmail.com>
2023-11-15 12:15:27 +0530

    Navigate with Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link
+            // Web pages are usually displayed as a stack,
+            // ie we can go back a page by pressing the back button
+            //* Using 'replace' clears the stack/history
+            // and back button takes you to the last page on the browser but not of the app's
+
+            //replace
+            href="/portfolio"
+          >
+            The Portfolio Page
+          </Link>
+        </li>
+        <li>
+          <Link href="/clients"> The Clients Page</Link>
+        </li>
+      </ul>
     </div>
   );
 }
